ede89c092ad45464b9a056d0df4abc905b385ea3
Dimitris Psathas <64214503+dimitris47@users.noreply.github.com>
2024-05-28 18:41:57 +0300

    add license

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+Copyright 2024 Dimitris Psathas <dimitrisinbox@gmail.com>
+This file is part of BellPlot.
+
+BellPlot is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License  as  published by  the  Free Software
+Foundation,  either version 3 of the License,  or (at your option)  any later
+version.
+
+BellPlot is distributed in the hope that it will be useful,  but  WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the  GNU General Public License  for more details.
+
+You should have received a copy of the  GNU General Public License along with
+BellPlot. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import sys
 from PySide6 import QtWidgets
 from mainwidget import MainWidget
